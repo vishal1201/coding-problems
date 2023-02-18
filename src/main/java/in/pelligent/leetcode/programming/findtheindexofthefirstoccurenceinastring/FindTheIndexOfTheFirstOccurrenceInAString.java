54e169497b89d8ed728d61575745e7c965d9031f
vishal1201 <vishalzambare5@gmail.com>
2023-02-18 11:56:49 +0530

    Add FindTheIndexOfTheFirstOccurrenceInAString

---
+++ src/main/java/in/pelligent/leetcode/programming/findtheindexofthefirstoccurenceinastring/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package in.pelligent.leetcode.programming.findtheindexofthefirstoccurenceinastring;
+
+class FindTheIndexOfTheFirstOccurrenceInAString {
+    public int solution1(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
